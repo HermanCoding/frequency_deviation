f5690f6e75fec113ec1f554455a2df3accf3357f
David H Squeed <david.hermansson@squeed.com>
2023-11-10 19:05:52 +0100

    refactored and added log

---
+++ merge_clean_data.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+data = []
+
+def merge_data(files_dir):
+    for dirpath, dirnames, filenames in os.walk(files_dir):
+        for filename in filenames:
+            if filename.endswith('.csv'):
+                print('Loading file {0}...'.format(filename))
+                data.append(pd.read_csv(os.path.join(dirpath, filename)))
