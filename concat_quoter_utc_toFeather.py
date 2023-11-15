98f601a148c7a3393bb3737bac83e9c329344d54
David H Squeed <david.hermansson@squeed.com>
2023-11-15 16:33:39 +0100

    UTC and feather

---
+++ concat_quoter_utc_toFeather.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import merge_clean_data
+
+
+class ConcatQuoterUtcToFeather:
+    def __init__(self, folder_name, file_name):
+        self.folder_name = folder_name
+        self.file_name = file_name
+
+    def concat_quoter_utc(self):
+        merge_clean_data.merge_data(files_dir=r'./processed_files/')
+        data = merge_clean_data.data
+        df = pd.concat(data, ignore_index=True, join='inner')
+        print('Data concatenated')
+
+        df['Time'] = pd.to_datetime(df['Time'], utc=True)
+        print('Time data set to the datetime type with UTC')
+
+        df['Time'] = pd.to_datetime(df['Time']).dt.tz_convert('Europe/Stockholm')
+        print('UTC changed to Europe/Stockholm')
+
+        if not os.path.exists(self.folder_name):
+            os.makedirs(self.folder_name)
+
+        file_path = os.path.join(self.folder_name, self.file_name)
+        df.to_feather(file_path)
+        print('Binary feather file created of dataframe')
